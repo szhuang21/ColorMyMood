20a2a134375aad040dbbc53905f347094140f89b
Sophia Zhuang <44931711+szhuang21@users.noreply.github.com>
2024-03-19 10:21:24 -0500

    random changes

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import CalendarColorPicker from "./components/CalendarColorPicker";
+function App() {
+  return (
+    <div className="App">
+      <div>Color My Mood</div>
+      <CalendarColorPicker></CalendarColorPicker>
+    </div>
+  );
+}
+
+export default App;
